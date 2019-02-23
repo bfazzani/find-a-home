cbb72d199b1a0b5b226bb7c70ce8bdd1df20ddbf
zhangtimothy <37489234+zhangtimothy@users.noreply.github.com>
2019-02-23 15:18:03 -0600

    sliders added
    first use of sliders

--- gui.py
+++ gui.py
@@ -1,3 +1,24 @@
 import math as m
+import PySimpleGUI as sg
 
-print("hello world")
+# print("hello world")
+
+layout = [
+    [sg.Text("Welcome to Find A Home! We can recommend you a neighborhood based upon your preferences.\n       --------------------------------------------------------------------------------------------------------------------------------")],
+     [sg.Text('Please indicate how much you care about the quality of each of the following modes of transportation:')],
+    # [sg.Text('Name', size=(15, 1)), sg.InputText('name')],
+    # [sg.Text('Address', size=(15, 1)), sg.InputText('address')],
+    # [sg.Text('Phone', size=(15, 1)), sg.InputText('phone')],
+    [sg.Text("Walking:", size=(15, 1)),
+     sg.Slider(range=(1, 5), default_value=3, size=(20, 15), orientation='horizontal', font=('Helvetica', 12))],
+    [sg.Text("Biking:", size=(15, 1)),
+     sg.Slider(range=(1, 5), default_value=3, size=(20, 15), orientation='horizontal', font=('Helvetica', 12))],
+    [sg.Text("Public Transit:", size=(15, 1)),
+     sg.Slider(range=(1, 5), default_value=3, size=(20, 15), orientation='horizontal', font=('Helvetica', 12))],
+    [sg.Submit(), sg.Cancel()]
+]
+
+window = sg.Window('Simple data entry window').Layout(layout)
+button, values = window.Read()
+
+print(button, values[0], values[1], values[2])
